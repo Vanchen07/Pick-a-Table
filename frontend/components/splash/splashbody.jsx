c8d18636c50b4a88bebb2900c94c3563545f9fc9
vanessa chen <vanchen07@gmail.com>
2019-04-14 22:35:15 -0700

    add popular restaurants component

--- frontend/components/splash/splashbody.jsx
+++ frontend/components/splash/splashbody.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class SplashBody extends React.Component {
     render() {
         return(
@@ -59,7 +60,134 @@ class SplashBody extends React.Component {
                         <div className="page-main-content">
                             <div className="row-page-content">
                                 <div className="column-page-content">
-
+                                    <div id="react-root">
+                                        <div className="bar">
+                                            <div className="bar-subbar">
+                                            </div>
+                                        </div>
+                                        <section>
+                                            <div className="popular-restaurant-bar">
+                                                <div className="popular-restaurant-bar-title">
+                                                    <h2 className="popular-restaurant-heading">Popular restaurants in San Francisco</h2>
+                                                </div>
+                                                <span className="view-all-bar">
+                                                    <a className="view-all" target="_blank" href="/search">
+                                                        <span>View all</span>
+                                                    </a>
+                                                </span>
+                                            </div>
+                                            <div className="popular-restaurant-body">
+                                                <div className="popular-restaurant-main-container">
+                                                    <div className="popular-restaurant-main">
+                                                        <div className="popular-restaurant-list">
+                                                            <div className="popular-restaurant-list-item-first">
+                                                                <a target="_blank" href="#">
+                                                                    <div className="popular-restaurant-list-item-content">
+                                                                        <div className="absinthe-head">
+                                                                        </div>
+                                                                        <div className="absinthe-body">
+                                                                            <h3  className="absinthe-body-name">Absinthe Brasserie and Bar - SF</h3>
+                                                                            <div className="absinthe-body-attributes">
+                                                                                <span className="absinthe-body-attributes-cuisine">French</span>
+                                                                                <span className="absinthe-body-attributes-dollars">
+                                                                                    <span className="absinthe-body-attributes-dollars-signs">$$$</span>
+                                                                                </span>
+                                                                                <span className="absinthe-body-attributes-neighborhood">Civic Center</span>
+                                                                            </div>
+                                                                            <div className="absinthe-body-booked">
+                                                                                <div className="absinthe-body-booked-body">
+                                                                                    <div><i class="fas fa-chart-line"></i></div>
+                                                                                    <div className="absinthe-body-booked-text">Booked 134 times today</div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <div className="popular-restaurant-list-item">
+                                                                <a target="_blank" href="#">
+                                                                    <div className="popular-restaurant-list-item-content">
+                                                                        <div className="flour-head">
+                                                                        </div>
+                                                                        <div className="absinthe-body">
+                                                                            <h3 className="absinthe-body-name">Flour + Water</h3>
+                                                                            <div className="absinthe-body-attributes">
+                                                                                <span className="absinthe-body-attributes-cuisine">Italian</span>
+                                                                                <span className="absinthe-body-attributes-dollars">
+                                                                                    <span className="absinthe-body-attributes-dollars-signs">$$</span>
+                                                                                </span>
+                                                                                <span className="absinthe-body-attributes-neighborhood">Mission</span>
+                                                                            </div>
+                                                                            <div className="absinthe-body-booked">
+                                                                                <div className="absinthe-body-booked-body">
+                                                                                    <div><i class="fas fa-chart-line"></i></div>
+                                                                                    <div className="absinthe-body-booked-text">Booked 52 times today</div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <div className="popular-restaurant-list-item">
+                                                                <a target="_blank" href="#">
+                                                                    <div className="popular-restaurant-list-item-content">
+                                                                        <div className="cala-head">
+                                                                        </div>
+                                                                        <div className="absinthe-body">
+                                                                            <h3 className="absinthe-body-name">Cala</h3>
+                                                                            <div className="absinthe-body-attributes">
+                                                                                <span className="absinthe-body-attributes-cuisine">Mexican</span>
+                                                                                <span className="absinthe-body-attributes-dollars">
+                                                                                    <span className="absinthe-body-attributes-dollars-signs">$$$</span>
+                                                                                </span>
+                                                                                <span className="absinthe-body-attributes-neighborhood">Civic Center</span>
+                                                                            </div>
+                                                                            <div className="absinthe-body-booked">
+                                                                                <div className="absinthe-body-booked-body">
+                                                                                    <div><i class="fas fa-chart-line"></i></div>
+                                                                                    <div className="absinthe-body-booked-text">Booked 53 times today</div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <div className="popular-restaurant-list-item-last">
+                                                                <a target="_blank" href="#">
+                                                                    <div className="popular-restaurant-list-item-content">
+                                                                        <div className="le-head">
+                                                                        </div>
+                                                                        <div className="absinthe-body">
+                                                                            <h3 className="absinthe-body-name">Le P'tit Laurent</h3>
+                                                                            <div className="absinthe-body-attributes">
+                                                                                <span className="absinthe-body-attributes-cuisine">French</span>
+                                                                                <span className="absinthe-body-attributes-dollars">
+                                                                                    <span className="absinthe-body-attributes-dollars-signs">$$</span>
+                                                                                </span>
+                                                                                <span className="absinthe-body-attributes-neighborhood">Glen Park</span>
+                                                                            </div>
+                                                                            <div className="absinthe-body-booked">
+                                                                                <div className="absinthe-body-booked-body">
+                                                                                    <div><i class="fas fa-chart-line"></i></div>
+                                                                                    <div className="absinthe-body-booked-text">Booked 20 times today</div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
+                                        <section>
+                                            
+                                        </section>
+                                        <section>
+                                            
+                                        </section>
+                                    </div>
                                 </div>
                             </div>
                         </div>
