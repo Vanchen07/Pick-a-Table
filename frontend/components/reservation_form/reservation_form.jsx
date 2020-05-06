25ce8ab467e147d264cd80009cc5b649afb10fdf
Vanessa Chen <vanchen07@gmail.com>
2020-05-05 17:48:26 -0700

    refactor reservation form date logic

--- frontend/components/reservation_form/reservation_form.jsx
+++ frontend/components/reservation_form/reservation_form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
+// import 'react-dates/initialize';
+// import 'react-dates/lib/css/_datepicker.css';
 import swal from 'sweetalert';
 
 var moment = require('moment');
@@ -12,7 +12,7 @@ class ReservationForm extends React.Component {
     super(props);
     this.state = {
       party_size: '2',
-      date: moment(),
+      date: '',
       time_slot_id: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -31,9 +31,10 @@ class ReservationForm extends React.Component {
       restaurant_id: this.props.restaurant.id,
       party_size: this.state.party_size,
       time_slot_id: this.state.time_slot_id,
-      date: this.state.date.format()
+      date: this.state.date
     });
 
+    console.log(reservationParams);
 
     if(this.props.currentUserId){
       this.props.createReservation(reservationParams)
@@ -95,73 +96,103 @@ class ReservationForm extends React.Component {
     // }
     return (
       <div className="reservation-form-wrapper">
-          <div className="reservation-form-header">
-            <h3 className="form-header-title">
-              <span id="scroll-reservation" className="title-span">Make a reservation</span>
-            </h3>
-          </div>
-          <form className="reservation-form-body" onSubmit={this.handleSubmit}>
-            {this.renderErrors()}
-            <div className="form-input-fields">
-              <div className="input-wrapper">
-                <div className="input-box">
-                  <div className="input-party-wrapper">
-                    <div className="input-party-size">Party Size</div>
-                    <div className="input-party-size-selection">
-                      <i className="fas fa-angle-down caret"></i>
-                      <select className="res-party-selection-dropdown" value={this.state.party_size} onChange={this.update('party_size')}>
-                        {this.renderParty()}
-                      </select>
-                    </div>
-                  </div>
-                  <div className="input-date-time-wrapper">
-                    <div className="input-date">
-                      <div className="date-label">Date</div>
-                      <div className="date-dropdown">
-                        <i className="fas fa-angle-down caret"></i>
-                        <input className="date-dropdown" type="date" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <span className="time-input-buttons-wrapper">
-              <span className="time-input-buttons-span">
-                <div className="time-input-buttons-header">
-                  <span>Select a time</span>
-                </div>
-                <div className="time-input-buttons-body">
-                  <div>
-                  {this.props.restaurant.remaining_time_slots.map((timeSlot, i) => {
-                return (
-                    <div key={i} className="time-input-button-1" onClick={()=> {this.setState({time_slot_id: timeSlot.id})}} >
-                        <div className={`timeslot ${this.state.time_slot_id === timeSlot.id ? 'time-slot-button-selected' : ''}`}>
-                          {timeSlot.formatted_start_time}
-                        </div>
-                    </div>
-                )
-              })}
-                  </div>
-                </div>
-              </span>
+        <div className="reservation-form-header">
+          <h3 className="form-header-title">
+            <span id="scroll-reservation" className="title-span">
+              Make a reservation
             </span>
-            <div className="res-button-wrapper">
-              <button className="res-button">
-                <span>Find a Table</span>
-              </button>
-            </div>
-            <div className="form-footer">
-              <div className="form-footer-booked">
-                <div className="form-footer-icon"><i className="fas fa-chart-line"></i></div>
-                <div className="form-footer-text">
-                  <div className="form-footer-text-span">
-                    <span>Booked {Object.keys(this.props.reservations).length} times today</span>
+          </h3>
+        </div>
+        <form className="reservation-form-body" onSubmit={this.handleSubmit}>
+          {this.renderErrors()}
+          <div className="form-input-fields">
+            <div className="input-wrapper">
+              <div className="input-box">
+                <div className="input-party-wrapper">
+                  <div className="input-party-size">Party Size</div>
+                  <div className="input-party-size-selection">
+                    <i className="fas fa-angle-down caret"></i>
+                    <select
+                      className="res-party-selection-dropdown"
+                      value={this.state.party_size}
+                      onChange={this.update("party_size")}
+                    >
+                      {this.renderParty()}
+                    </select>
+                  </div>
+                </div>
+                <div className="input-date-time-wrapper">
+                  <div className="input-date">
+                    <div className="date-label">Date</div>
+                    <div className="date-dropdown">
+                      <i className="fas fa-angle-down caret"></i>
+                      <input
+                        className="date-dropdown"
+                        type="date"
+                        value={this.state.date}
+                        onChange={this.update("date")}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </form>
+          </div>
+          <span className="time-input-buttons-wrapper">
+            <span className="time-input-buttons-span">
+              <div className="time-input-buttons-header">
+                <span>Select a time</span>
+              </div>
+              <div className="time-input-buttons-body">
+                <div>
+                  {this.props.restaurant.remaining_time_slots.map(
+                    (timeSlot, i) => {
+                      return (
+                        <div
+                          key={i}
+                          className="time-input-button-1"
+                          onClick={() => {
+                            this.setState({ time_slot_id: timeSlot.id });
+                          }}
+                        >
+                          <div
+                            className={`timeslot ${
+                              this.state.time_slot_id === timeSlot.id
+                                ? "time-slot-button-selected"
+                                : ""
+                            }`}
+                          >
+                            {timeSlot.formatted_start_time}
+                          </div>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+              </div>
+            </span>
+          </span>
+          <div className="res-button-wrapper">
+            <button className="res-button">
+              <span>Find a Table</span>
+            </button>
+          </div>
+          <div className="form-footer">
+            <div className="form-footer-booked">
+              <div className="form-footer-icon">
+                <i className="fas fa-chart-line"></i>
+              </div>
+              <div className="form-footer-text">
+                <div className="form-footer-text-span">
+                  <span>
+                    Booked {Object.keys(this.props.reservations).length} times
+                    today
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
