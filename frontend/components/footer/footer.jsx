c69817118b8c2f965baee902249b902b516dad6c
Vanessa Chen <vanchen07@gmail.com>
2020-09-05 17:19:52 -0700

    update footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,6 +5,10 @@ class Footer extends React.Component {
     render() {  
         return(
             <div className='splash-footer'>
+                <div className="footer-text">
+                    <img src={window.logo} className="footer-logo"/>
+                    Pick-a-Table was inspired by the restaurant reservation app OpenTable. This project was created purely for educational purposes. All images used are fron Unsplash.
+                </div>
                 <h6 className="footer-heading" >Follow me on</h6>
                 <div className="footer-social">
                     <div className="footer-social-icons">
